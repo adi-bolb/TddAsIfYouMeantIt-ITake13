d9829fc2aec6286dc16eafa9444d0c7b2fb2d785
Adi Bolboaca <adi_bolb123@yahoo.com>
2013-05-31 13:02:27 +0300

    Created a setup method

--- TddAsIfYouMeantItITake/src/Something.java
+++ TddAsIfYouMeantItITake/src/Something.java
@@ -1,33 +1,45 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Something {
+	
+	private String emptyBoard;
+	private String oneByOneBoard;
+
+	@Before
+	public void setup(){
+		oneByOneBoard = emptyOneByOneBoard();
+		emptyBoard = "";
+	}
+	
 	@Test
 	public void TicTacToeWithZeroBoardWillMakeXWinAlways() {
-		String boardZero = "";
 		String expectXToWin = "x-win";
-		String actual = winner(boardZero);
+		String actual = winner(emptyBoard);
 		
 		assertEquals(expectXToWin, actual);
 	}
 	
 	@Test
 	public void testNobodyWinsWithoutPlayingOnOneByOneBoard() {
-		String boardOneByOne = "empty board one by one";
 		String expectNobodyToWin = "pending";
-		String actual = gameStatus(boardOneByOne);
+		String actual = gameStatus(oneByOneBoard);
 		
 		assertEquals(expectNobodyToWin, actual);
 	}
 	
 	@Test
 	public void xPlaysFirstOnOneByOneBoard () throws Exception {
-		String boardOneByOne = "empty board one by one";
 		String nextPlayer = "x";
 		String actualNextPlayer = "x";
 		
 		assertEquals(nextPlayer, actualNextPlayer);
+	}
+
+	private String emptyOneByOneBoard() {
+		return "empty board one by one";
 	}
 	
 	private String gameStatus(String boardOneByOne) {
