93fbe961bee2ef8d29fb1116a5061fbcedd4f965
Adi Bolboaca <adi_bolb123@yahoo.com>
2013-05-31 12:16:29 +0300

    first green test with simplest tic tac toe game

---
+++ TddAsIfYouMeantItITake/src/Something.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class Something {
+
+	@Test
+	public void TestSmth(){
+
+		String boardZero = "";
+		String expectXToWin = "true";
+		
+		String actual = "true";
+		
+		assertEquals(expectXToWin, actual);
+	
+	}
+	
+}
